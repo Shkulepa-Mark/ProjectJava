d6530f94f393f8cd6fb8e78c409f88f9a325bd73
shkulepa mark <skulepamark@gmail.com>
2024-11-15 23:27:20 +0300

    Added clinic imitation

---
+++ src/main/java/GL6_VAR_7A/ClinicImitation.java
@@ -0,0 +1,20 @@
+package GL6_VAR_7A;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClinicImitation {
+    public static void main(String[] args) {
+        Doctor surgeon = new Surgeon();
+        Doctor neurosurgeon = new Neurosurgeon();
+
+        List<Doctor> doctors = new ArrayList<>();
+        doctors.add(surgeon);
+        doctors.add(neurosurgeon);
+
+        for (Doctor doctor : doctors) {
+            doctor.treat();
+            doctor.consult();
+        }
+    }
+}
