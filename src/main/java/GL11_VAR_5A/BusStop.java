8ff159c3d8c57f4bde4e4e4e79313a4bbbfd8860
shkulepa mark <skulepamark@gmail.com>
2024-11-25 20:15:59 +0300

    Implemented bus stop logic with limited capacity

---
+++ src/main/java/GL11_VAR_5A/BusStop.java
@@ -0,0 +1,34 @@
+package GL11_VAR_5A;
+
+import java.util.concurrent.Semaphore;
+
+public class BusStop {
+    private final String name;
+    private final Semaphore semaphore;
+
+    public BusStop(String name, int capacity) {
+        this.name = name;
+        this.semaphore = new Semaphore(capacity, true);
+    }
+
+    public boolean arrive(String busName) {
+        try {
+            if (semaphore.tryAcquire()) {
+                System.out.println(busName + " stop on " + name);
+                Thread.sleep(1000);
+                return true;
+            } else {
+                System.out.println(busName + " couldn't stop on " + name + ": no places.");
+                return false;
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            System.out.println(busName + " interrupted by " + name);
+            return false;
+        }
+    }
+
+    public void leave(String busName) {
+        System.out.println(busName + " leaves " + name);
+    }
+}
