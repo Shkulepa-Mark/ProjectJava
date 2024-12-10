6344cca6536f6b93446f24bbe931d51d9a49fef9
shkulepa mark <skulepamark@gmail.com>
2024-12-10 22:27:31 +0300

    Devided Bus and BusStop as separate classes

--- src/main/java/busSimulation/BusStop.java
+++ src/main/java/busSimulation/BusStop.java
@@ -34,34 +34,3 @@ public class BusStop {
     }
 }
 
-class Bus extends Thread {
-    private final String busName;
-    private final BusStop[] route;
-
-
-    Bus(String busName, BusStop[] route) {
-        this.busName = busName;
-        this.route = route;
-    }
-
-    @Override
-    public void run() {
-        for (BusStop stop : route) {
-            if (stop.arrive(busName)) {
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                } finally {
-                    stop.leave(busName);
-                }
-            }
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
-        System.out.println(busName + " finished the route");
-    }
-}
